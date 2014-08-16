80d66c4237003bca0dfa737926e6965eca886f1c
Russell Sim <russell.sim@gmail.com>
2014-08-16 10:45:45 +1000

    Convert last compare tests to evaluation tests
    This rewritten test executes the whwords main function and confirms that
    the correct values are returned.

--- snappy/tests/test_parser.py
+++ snappy/tests/test_parser.py
@@ -1,8 +1,5 @@
 from os import path
-import difflib
 import unittest
-
-import astor
 
 from snappy import tests
 from snappy import parser
@@ -385,91 +382,7 @@ class TestArgumentParser(unittest.TestCase):
     pass
 
 
-class TestBlockParser(unittest.TestCase):
+class TestWHWordsFunction(tests.BlockParser, unittest.TestCase):
+    xml = open(path.join(SAMPLE_PROGRAMS, 'wh_words.xml')).read()
 
-    def setUp(self):
-        super(TestBlockParser, self).setUp()
-
-    def assertAST(self, ast, string):
-        ast_string = astor.to_source(ast)
-        try:
-            self.assertEqual(ast_string, string)
-        except:
-            for line in difflib.unified_diff(ast_string.split('\n'),
-                                             string.split('\n'),
-                                             n=100):
-                print line
-            raise
-
-    def test_wh_words_parser(self):
-        filename = path.join(SAMPLE_PROGRAMS, 'wh_words.xml')
-        p = parser.parse(filename)
-        ctx = p.create_context()
-        self.assertTrue(len(p.scripts(ctx)) == 2, p.scripts)
-        self.assertTrue(len(p.custom_blocks) == 29, p.custom_blocks)
-
-    def test_wh_words_function(self):
-        filename = path.join(SAMPLE_PROGRAMS, 'wh_words.xml')
-        p = parser.parse(filename)
-        ctx = p.create_context()
-        ast = p.custom_blocks["wh-words %s"].to_ast(ctx)
-
-        self.assertAST(ast,
-                       '''def wh_words_words_(words):
-    _vars = {}
-    _vars['result'] = stdlib.doReport([])
-    for word in words:
-        if stdlib.doReport((stdlib.doReport(stdlib.equals(stdlib.doReport(word[(1 - 1)]), 'w')) and stdlib.doReport(stdlib.equals(stdlib.doReport(word[(2 - 1)]), 'h')))):
-            _vars['result'].append(word)
-    return stdlib.doReport(_vars['result'])''')
-
-    def test_for_function(self):
-        filename = path.join(SAMPLE_PROGRAMS, 'wh_words.xml')
-        p = parser.parse(filename)
-        ctx = p.create_context()
-        ast = p.custom_blocks["for %upvar = %n to %n %cs"].to_ast(ctx)
-        self.assertAST(ast,
-                       '''def for_i_start_to_end_action_(i, start, end, action):
-    _vars = {}
-    if stdlib.doReport((start > end)):
-        _vars['step'] = (-1)
-        _vars['tester'] = (lambda : stdlib.doReport((i < end)))
-    else:
-        _vars['step'] = 1
-        _vars['tester'] = (lambda : stdlib.doReport((i > end)))
-    i = start
-    while (not _vars['tester']()):
-        action(i, start, end, action)
-        i = (i + _vars['step'])''')
-
-    def test_sentence_list(self):
-        filename = path.join(SAMPLE_PROGRAMS, 'wh_words.xml')
-        p = parser.parse(filename)
-        ctx = p.create_context()
-        ast = p.custom_blocks["sentence->list %txt"].to_ast(ctx)
-        self.assertAST(ast,
-                       '''def sentence_list_text_(text):
-    _vars = {}
-
-    def custom_block_0(i, start, end, action):
-        if stdlib.doReport(stdlib.equals(stdlib.doReport(text[(i - 1)]), ' ')):
-            if stdlib.doReport((not stdlib.doReport(stdlib.equals(_vars['thisword'], _vars['emptyword'])))):
-                _vars['result'].append(_vars['thisword'])
-                _vars['thisword'] = _vars['emptyword']
-        else:
-            _vars['thisword'] = stdlib.doReport((_vars['thisword'] + stdlib.doReport(text[(i - 1)])))
-    _vars['result'] = stdlib.doReport([])
-    _vars['thisword'] = ''
-    _vars['emptyword'] = ''
-    for_i_start_to_end_action_('i', 1, stdlib.doReport(len(text)), custom_block_0)
-    if stdlib.doReport((not stdlib.doReport(stdlib.equals(_vars['thisword'], _vars['emptyword'])))):
-        _vars['result'].append(_vars['thisword'])
-    return stdlib.doReport(_vars['result'])''')
-
-    # def test_wh_words_render_file(self):
-
-    #     filename = path.join(SAMPLE_PROGRAMS, 'wh_words.xml')
-    #     p = parser.parse(filename)
-    #     ctx = p.create_context()
-    #     file_ast = p.to_ast(ctx, 'main_0')
-    #     print astor.to_source(file_ast)
+    report = {'result': [u'whoever', u'Who', u'What.', u'What']}
